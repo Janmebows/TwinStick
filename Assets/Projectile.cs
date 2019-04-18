fc8805023c419b5b5e7bf9603d95edc71eb37349
Andrew Martin <Janmebows@gmail.com>
2019-04-18 14:12:12 +0930

    Bullets???

---
+++ Assets/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 pos;
+    Vector3 dir;
+    float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Init(Vector3 _pos, Vector3 _dir,float _force){
+        pos = _pos;
+        dir = _dir;
+        force = _force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject owner;
+ 
+    float time =0;
+
+    public float fireTime;
+    public Transform bulletSpawnPoint;
+    public GameObject bullet;
+    public float bulletForce;
+    public float accuracy = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(owner==null)
+            owner=transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > time+fireTime){
+            FireBullet();
+            time+=fireTime;
+        }
+        
+    }
+
+
+    void FireBullet(){
+        GameObject bull = Instantiate(bullet,bulletSpawnPoint.position,bulletSpawnPoint.rotation);
+        Rigidbody brigid = bull.GetComponent<Rigidbody>();
+        Collider bcoll = bull.GetComponent<Collider>();
+        Physics.IgnoreCollision(owner.GetComponent<Collider>(),bcoll,true);
+        brigid.AddForce(bulletForce*bulletSpawnPoint.forward);
+        Destroy(bull,3f);
+        
+    }
+}
